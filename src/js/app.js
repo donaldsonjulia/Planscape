c14dda8e2ae0f622cc25b0f2923c33fb9300a3d6
SamDC <sam.davis.castro@gmail.com>
2016-11-04 11:22:11 -0400

    commit to sam branch on new repo

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,22 @@
-angular.module('planscape', ['ui.router', 'LocalStorageModule']).config(function($stateProvider, $urlRouterProvider) {
+angular.module('planscape', ['ui.router', 'LocalStorageModule', 'google-maps']).config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise('create');
+    $urlRouterProvider.otherwise('create');
 
-  $stateProvider.state('main', {
-    url: '/',
-    abstract: true,
-    template: '<ui-view></ui-view>'
-  }).state('main.login', {
-    url: 'login',
-    templateUrl: './src/templates/login.html',
-    controller: 'LoginController as login'
-  }).state('main.create', {
-    url: 'create',
-    templateUrl: './src/templates/create.html',
-    controller: 'CreateRouteController as create'
-  }).state('main.route', {
-    url: 'route',
-    templateUrl: './src/templates/route.html',
-    controller: 'CreateRouteController as create'
-  }).state('main.map', {
-    url: 'map',
-    templateUrl: './src/templates/map.html',
-    controller: 'MapController as map'
-  });
-
+    $stateProvider.state('main', {
+        url: '/',
+        abstract: true,
+        template: '<ui-view></ui-view>'
+    }).state('main.login', {
+        url: 'login',
+        templateUrl: './src/templates/login.html',
+        controller: 'LoginController as login'
+    }).state('main.create', {
+        url: 'create',
+        templateUrl: './src/templates/create.html',
+        controller: 'CreateRouteController as create'
+    }).state('main.map', {
+        url: 'map',
+        templateUrl: './src/templates/map.html',
+        controller: 'MapController as mapCtrl'
+    });
 });
