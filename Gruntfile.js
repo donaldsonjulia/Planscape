a72ba51a9b18f63f5002325dd5a6b8f9c21e36e7
SamDC <sam.davis.castro@gmail.com>
2016-11-07 14:11:54 -0500

    Added logo, further stylized map directions (step-by-step ones that get displayed below the map)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,90 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed',
-                    loadPath: [
-                        'node_modules/bourbon/app/assets/stylesheets',
-                        'node_modules/bourbon-neat/app/assets/stylesheets',
+   grunt.initConfig({
+       pkg: grunt.file.readJSON('package.json'),
+       sass: {
+           dist: {
+               options: {
+                   style: 'compressed',
+                   loadPath: [
+                       'node_modules/bourbon/app/assets/stylesheets',
+                       'node_modules/bourbon-neat/app/assets/stylesheets',
 
-                    ]
-                },
-                files: {
-                    'lib/styles/app.min.css': 'src/styles/app.scss'
-                }
-            }
-        },
+                   ]
+               },
+               files: {
+                   'lib/styles/app.min.css': 'src/styles/app.scss'
+               }
+           }
+       },
 
-        uglify: {
-            options: {
-                preserveComments: false
-            },
-            my_target: {
-                files: {
-                    'lib/js/vendor.min.js': ['lib/js/vendor.js']
-                }
-            }
-        },
+       uglify: {
+           options: {
+               preserveComments: false
+           },
+           my_target: {
+               files: {
+                   'lib/js/vendor.min.js': ['lib/js/vendor.js']
+               }
+           }
+       },
 
-        concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
-                files: {
-                    'lib/js/vendor.js': ['src/js/vendor/*.js']
-                }
-            }
-        },
+       concat: {
+           options: {
+               separator: ';'
+           },
+           dist: {
+               files: {
+                   'lib/js/vendor.js': ['src/js/vendor/*.js']
+               }
+           }
+       },
 
-        jshint: {
-            all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
-        },
+       jshint: {
+           all: ['Gruntfile.js', 'src/*.js', 'test/*.js']
+       },
 
-        mochaTest: {
-            test: {
-                options: {
-                    reporter: 'spec',
-                    captureFile: 'test/results.txt',
-                    quiet: false,
-                    clearRequireCache: false,
-                    noFail: false
-                },
-                src: ['test/**/*.js']
-            }
-        },
+       mochaTest: {
+           test: {
+               options: {
+                   reporter: 'spec',
+                   captureFile: 'test/results.txt',
+                   quiet: false,
+                   clearRequireCache: false,
+                   noFail: false
+               },
+               src: ['test/**/*.js']
+           }
+       },
 
 
-        watch: {
-            css: {
-                files: ['src/styles/**/*'],
-                tasks: ['sass']
-            },
+       watch: {
+           css: {
+               files: ['src/styles/**/*'],
+               tasks: ['sass']
+           },
 
-            javascript: {
-                files: [
-                    'src/js/vendor/*', 'test/*.js'
-                ],
-                tasks: ['mochaTest', 'jshint', 'concat', 'uglify']
-            }
+           javascript: {
+               files: [
+                   'src/js/vendor/*', 'test/*.js'
+               ],
+               tasks: ['mochaTest', 'jshint', 'concat', 'uglify']
+           }
 
-        }
-    });
+       }
+   });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.registerTask('default', [
-        'sass',
-        'watch',
-        'concat',
-        'uglify',
-        'jshint',
-        'mochaTest',
-    ]);
+   grunt.loadNpmTasks('grunt-contrib-sass');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-mocha-test');
+   grunt.registerTask('default', [
+       'sass',
+       'watch',
+       'concat',
+       'uglify',
+       'jshint',
+       'mochaTest',
+   ]);
 };
